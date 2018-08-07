53f70a2eb7211016f0181de4ab2b95f58fa4b8b7
JrPtn <jrptnr13@gmail.com>
2018-08-07 13:17:17 -0500

    FPSController and FPSMovement Additions
    Adding the scripts for the controller and movement scripts for player
    movement and mobility.

---
+++ FPSController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*-------------------------- FPSController: MonoBehaviour -------------------
+   |  Purpose:  Performs the necessary calls to the FPSMovement script to perform  
+   |            movement, forces, jumps, etc.     
+   |
+   |  Author: Jordan Pitner
+   |  Date: August 7th, 2018
+   *-------------------------------------------------------------------*/
+[RequireComponent(typeof(Rigidbody))]
+public class FPSController : MonoBehaviour {
+
+    #region Global Variables
+
+    [SerializeField]
+    private float speed = 5f;
+
+    [SerializeField]
+    private float sprintSpeed = 8f;
+
+    [SerializeField]
+    private float jumpForce = 2.0f; 
+
+    [SerializeField]
+    private float lookSensitivity = 5f;
+
+    private bool isGrounded; 
+
+    private FPSMovement movement;
+
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        movement = GetComponent<FPSMovement>();	
+        
+	}
+	
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 velocity; 
+
+        // Calculate movement as 3D vector
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float yMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * yMov;
+
+        if (Input.GetKey(KeyCode.LeftShift) == true)
+        {
+            velocity = (movHorizontal + movVertical).normalized * sprintSpeed;
+        }
+        else
+        {
+            velocity = (movHorizontal + movVertical).normalized * speed;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            movement.Jump(jumpForce);
+            isGrounded = false;
+        }
+
+        // Apply movement
+        movement.Move(velocity);
+
+        // Calculate rotation as a new vector
+        float yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 rotation = new Vector3(0f, yRot, 0f) * lookSensitivity;
+        movement.Rotate(rotation);
+
+        // Calculate camera rotation as new future euler angles
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        float cameraRotationX = xRot * lookSensitivity;
+        movement.RotateCamera(cameraRotationX);
+    }
+}
---
+++ FPSMovement.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*-------------------------- FPSMovement: MonoBehaviour -------------------
+   |  Purpose:  Performs and sets the necessary variables, forces, etc for 
+   |            the player to be able to move and interact
+   |
+   |  Author: Jordan Pitner
+   |  Date: August 7th, 2018
+   *-------------------------------------------------------------------*/
+[RequireComponent(typeof(Rigidbody))]
+public class FPSMovement : MonoBehaviour {
+
+    // Global Variables
+    #region Global Variables
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+
+    [SerializeField]
+    private float cameraRotationLimit = 85;
+    private float cameraRotationX = 0f;
+    private float currentCameraRotationX = 0f;
+    private float jumpForce = 0f; 
+
+    private Rigidbody rigidBody;
+    public Camera cam;
+
+    #endregion
+
+    /*-------------------------- Start() -------------------
+   |  Function Start
+   |
+   |  Purpose:  Function to be called when the script first starts, sets globals
+   |
+   |  Parameters: None
+   |	      
+   |  Returns:  Nothing
+   *-------------------------------------------------------------------*/
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    /*-------------------------- Move(Vector3 velocity) -------------------
+    |  Function Move
+    |
+    |  Purpose:  Set the velocity of the player movement
+    |
+    |  Parameters: Vector3 velocity
+    |	      
+    |  Returns:  Nothing
+    *-------------------------------------------------------------------*/
+    public void Move(Vector3 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+
+    /*-------------------------- Rotate(Vector3 rotation) -------------------
+    |  Function Rotate
+    |
+    |  Purpose:  Set the rotation of the player movement
+    |
+    |  Parameters: Vector3 rotation
+    |	      
+    |  Returns:  Nothing
+    *-------------------------------------------------------------------*/
+    public void Rotate(Vector3 rotation)
+    {
+        this.rotation = rotation; 
+    }
+
+
+    /*-------------------------- RotateCamera(Vector3 cameraRotationX) -------------------
+   |  Function RotateCamera
+   |
+   |  Purpose:  Set the rotation of the camera movement
+   |
+   |  Parameters: Vector3 cameraRotationX
+   |	      
+   |  Returns:  Nothing
+   *-------------------------------------------------------------------*/
+    public void RotateCamera(float cameraRotationX)
+    {
+        this.cameraRotationX = cameraRotationX;
+    }
+
+
+    /*-------------------------- Jump(float jumpForce) -------------------
+   |  Function Jump
+   |
+   |  Purpose:  Set the jumpForce float variable for player jumping
+   |
+   |  Parameters: float jumpForce
+   |	      
+   |  Returns:  Nothing
+   *-------------------------------------------------------------------*/
+    public void Jump(float jumpForce)
+    {
+        this.jumpForce = jumpForce;
+        PerformJump();
+    }
+
+
+    /*-------------------------- FixedUpdate() -------------------
+   |  Function FixedUpdate
+   |
+   |  Purpose:  Fixed calls to update based on frames, perform movement 
+   |            and rotation functions consistently 
+   |
+   |  Parameters: None
+   |	      
+   |  Returns:  Nothing
+   *-------------------------------------------------------------------*/
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+
+    /*-------------------------- PerformMovement() -------------------
+   |  Function PerformMovement
+   |
+   |  Purpose:  Perform the actual player movement on the rigid body
+   |
+   |  Parameters: None
+   |	      
+   |  Returns:  Nothing
+   *-------------------------------------------------------------------*/
+    private void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rigidBody.MovePosition(rigidBody.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+
+    /*-------------------------- PerformRotation() -------------------
+   |  Function PerformRotation
+   |
+   |  Purpose:  Perform the actual rotation movement on the player rigid body
+   |
+   |  Parameters: None
+   |	      
+   |  Returns:  Nothing
+   *-------------------------------------------------------------------*/
+    private void PerformRotation()
+    {
+        rigidBody.MoveRotation(rigidBody.rotation * Quaternion.Euler(rotation));
+
+        // Camera rotation calculation
+        if (cam != null)
+        {
+            // Set the rotation and clamp it to limit rotation angles
+            currentCameraRotationX -= cameraRotationX;
+            currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+            // Apply the rotation transform to the player camera
+            cam.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+        }
+    }
+
+
+    /*-------------------------- PerformJump() -------------------
+   |  Function PerformJump
+   |
+   |  Purpose:  Perform the actual jump and apply upward force to the player rigid body
+   |
+   |  Parameters: None
+   |	      
+   |  Returns:  Nothing
+   *-------------------------------------------------------------------*/
+    private void PerformJump()
+    {
+        Vector3 jump = new Vector3(0f, 2.0f, 0f);
+
+        rigidBody.AddForce(jump * jumpForce, ForceMode.Impulse);
+    }
+
+}
